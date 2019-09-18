1cb70e93781110ba240dcafbe21a3c1ed8d594a2
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-09-18 01:03:11 -0500

    added hardcoded symbol file load to driver

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Assembler
         {
             Console.WriteLine("Hello World!");
             SymbolTable symbolTable = new SymbolTable();
-
+            symbolTable.LoadSymbols("../../../SYMBOLS.DAT");
+            symbolTable.Print();
+            Console.WriteLine();
 
             Console.WriteLine("Please enter a file path: ");
             string filePath = Console.ReadLine();
