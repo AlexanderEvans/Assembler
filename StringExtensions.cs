0daa07737ab09c204c75d95c9e2ac2f2aa3db669
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-09-18 01:04:31 -0500

    Added Whitespace trim && compactor

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -20,5 +20,62 @@ namespace Assembler
             }
             return count;
         }
+
+        public static String CompactAndTrimWhitespaces(this string str)
+        {
+            return CompactWhitespaces(new StringBuilder(str)).ToString();
+        }
+
+        public static StringBuilder CompactWhitespaces(StringBuilder sb)
+        {
+            if (sb.Length != 0)
+            {
+                int first;
+                for (first = 0; first < sb.Length && Char.IsWhiteSpace(sb[first]); first++)
+                {
+
+                }
+
+                // if sb has only whitespaces, then return empty string
+                if (first == sb.Length)
+                {
+                    sb.Length = 0;
+                }
+                else
+                {
+                    // set [end] to last not-whitespace char
+
+                    int last;
+                    for (last = sb.Length - 1; last >= 0 && Char.IsWhiteSpace(sb[last]); last--)
+                    {
+
+                    }
+                    // compact string
+
+                    int current = 0;
+                    bool previousIsWhitespace = false;
+
+                    for (int i = first; i <= last; i++)
+                    {
+                        if (Char.IsWhiteSpace(sb[i]) && previousIsWhitespace!=true)
+                        {
+                            previousIsWhitespace = true;
+                            sb[current] = ' ';
+                            current++;
+                        }
+                        else if(Char.IsWhiteSpace(sb[i])==false)
+                        {
+                            previousIsWhitespace = false;
+                            sb[current] = sb[i];
+                            current++;
+                        }
+                    }
+
+                    sb.Length = current;
+                }
+            }
+
+            return sb;
+        }
     }
 }
