b58e3de3d76bd6dbfd81c0c18f38c32d8316f2bc
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-09-18 02:45:01 -0500

    Formatted driver console output

--- Program.cs
+++ Program.cs
@@ -14,22 +14,21 @@ namespace Assembler
             Console.WriteLine("Current symbol table values after loading:");
             symbolTable.Print();
             Console.WriteLine();
+            Console.WriteLine();
 
-
-            Console.WriteLine(args.Length);
-            foreach (string arg in args)
-                Console.WriteLine(arg);
             string filePath;
             if (args.Length==0)
             {
-                Console.WriteLine("Please enter the search file path: ");
+                Console.WriteLine("Please enter the search file name: ");
                 filePath = Console.ReadLine();
             }
             else
             {
                 filePath = args[0];
             }
-            symbolTable.SearchSymbols(filePath);
+            Console.WriteLine();
+            Console.WriteLine();
+            symbolTable.SearchSymbols("../../../" + filePath);
         }
     }
 }
