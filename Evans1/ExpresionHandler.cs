695a8fa150f1c458de9b5d0336cb7fd399259725
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-10-06 05:49:24 -0500

    added arithmatic expresion detection

--- Evans1/ExpresionHandler.cs
+++ Evans1/ExpresionHandler.cs
@@ -6,10 +6,25 @@ namespace Evans1
 {
     class ExpresionHandler
     {
-        //static bool ResolveExpresion(SymbolTable symbolTable, string currentLine, out Globals.Symbol? symbol)
-        //{
-        //    symbol = null;
+        static bool ResolveExpresion(SymbolTable symbolTable, string expresion, string currentLine, out Globals.Symbol? symbol)
+        {
+            symbol = null;
+            int arithmaticOperatorCount = expresion.CountStringCharachters(out List<char> found, '+', '-');
+            if (arithmaticOperatorCount <= 1)
+            {
+                if(found.Contains('+'))
+                {
 
-        //}
+                }
+                else if(found.Contains('-'))
+                {
+
+                }
+            }
+            else
+            {
+                Debug.LogError("Invalid expresion.  There is more than one arithmatic operator indicating 3 or more terms.", "Resolving Expresion");
+            }
+        }
     }
 }
