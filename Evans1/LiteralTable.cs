666631823f3bdaf00c19fc178943ecdd75a8209e
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-10-06 23:52:27 -0500

    added literal value struct

--- Evans1/LiteralTable.cs
+++ Evans1/LiteralTable.cs
@@ -6,5 +6,12 @@ namespace Evans1
 {
     class LiteralTable
     {
+        struct LiteralValue
+        {
+            public string label;
+            public string value;
+            public bool isChar;
+            public int address;
+        }
     }
 }
