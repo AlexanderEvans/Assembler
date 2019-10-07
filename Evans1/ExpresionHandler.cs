6cf59b185ad551f20bfb407fe1ccb43c35d3ea1b
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-10-06 23:58:33 -0500

    implemented number parser

--- Evans1/ExpresionHandler.cs
+++ Evans1/ExpresionHandler.cs
@@ -30,6 +30,25 @@ namespace Evans1
             {
                 Debug.LogError("Invalid expresion.  There is more than one arithmatic operator indicating 3 or more terms.", "Resolving Expresion");
             }
+        bool ParseNum(string digits, out Globals.Symbol? sym, string currentLine="")
+        {
+            bool rtnVal = true;
+            Globals.Symbol tmp = default;
+            tmp.label = digits;
+            tmp.RFlag = false;
+            tmp.MFlag = false;
+            tmp.IFlag = true;
+            if (int.TryParse(digits, out tmp.value) != true)
+            {
+                rtnVal = false;
+                Debug.LogError("Attempted to resolve invalid expresion!\n\tArithmitic must be performed on exactly 2 terms, skipping: " + currentLine, "Resolving Expresion");
+            }
+
+            if (rtnVal==true)
+                sym = tmp;
+            else
+                sym = null;
+            return rtnVal;
         }
     }
 }
